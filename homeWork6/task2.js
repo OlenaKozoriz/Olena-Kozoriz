02823c21004bc7b6d3887b523f785b453b279d0d
Olena Kozoriz <lenchik13@ukr.net>
2024-05-14 10:59:46 +0300

    Home Work 6

---
+++ homeWork6/task2.js
@@ -0,0 +1,10 @@
+function Num (num) {
+	console.log (num);
+	if (num <= 0) {
+		return "Stop"
+	} else {
+		return Num (num-1)
+}
+}
+const result = Num (5);
+console.log (result)
---
+++ homeWork6/task3.js
@@ -0,0 +1,66 @@
+function divide (numerator, denominator) {
+	
+	const result = numerator/denominator;
+	if (denominator==0) {
+		throw new Error ("Ділення на 0 неприпустиме")
+	} else if (typeof numerator!=='number') {
+		throw new Error ("This is wrong combination of arguments")
+	} else if ( typeof denominator !=='number') {
+		throw new Error ("This is wrong combination of arguments")
+	}
+	return result	
+};
+
+try {
+	let finalResult1 = divide (10, 0);
+	console.log (finalResult1)
+} catch (error) {
+	console.error ("Denominator cannot be 0.", error.message)
+} finally {
+	console.log ("Роботу завершено")
+};
+
+try {
+	let finalResult2 = divide (25, 5);
+    console.log (finalResult2);
+} catch (error) {
+	console.error ("This is wrong combination of arguments", error.message)
+} finally {
+	console.log ("Роботу завершено")
+};
+
+try {
+	let finalResult3 = divide ('', 5);
+    console.log (finalResult3);
+} catch (error) {
+	console.error ("Denominator cannot be a space.", error.message)
+} finally {
+	console.log ("Роботу завершено")
+};
+
+try {
+	let finalResult4 = divide (undefined, 5);
+    console.log (finalResult4);
+} catch (error) {
+	console.error ("Denominator cannot be an undefined value.", error.message)
+} finally {
+	console.log ("Роботу завершено")
+};
+
+try {
+	let finalResult4 = divide (12, );
+    console.log (finalResult4);
+} catch (error) {
+	console.error ("Nominator should be a number.", error.message)
+} finally {
+	console.log ("Роботу завершено")
+};
+
+try {
+	let finalResult5 = divide (368, 27);
+    console.log (finalResult5);
+} catch (error) {
+	console.error ("This is wrong combination of arguments", error.message)
+} finally {
+	console.log ("Роботу завершено")
+}
