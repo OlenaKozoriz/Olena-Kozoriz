daab5dcc98d544bd9f1f08d5fca5cffe46d94a15
Olena Kozoriz <lenchik13@ukr.net>
2024-05-20 15:08:28 +0300

    Home Work 8 'Objects'

---
+++ homeWork8/task2.js
@@ -0,0 +1,9 @@
+const book = {
+	title: 'Dandelion Wine',
+	author: 'Ray Bradbury',
+	year: 1957
+}
+
+const {title: bookTitle, author: bookAuthor} = book;
+	
+console.log(bookTitle, bookAuthor)
---
+++ homeWork8/task5.js
@@ -0,0 +1,14 @@
+const students = [
+	{name: 'Olena', age: 17, faculty: 'Electronics'},
+	{name: 'Hanna', age: 18, faculty: 'Economics'},
+	{name: 'Anactaciia', age: 18, faculty: 'Linguistics'},
+	{name: 'Dmytro', age: 17, faculty: 'Art'}
+]
+
+for (const student of students) {
+	console.log (student)
+}
+//Destructuring inside of the cycle
+for (const {name: studentName, age: studentAge, faculty: studentFaculty} of students) {
+	console.log (studentName, studentAge, studentFaculty)
+}
