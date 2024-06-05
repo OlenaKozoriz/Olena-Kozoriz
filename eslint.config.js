290e91840f02b47b5620b2e81f38ca80fae88873
Olena Kozoriz <lenchik13@ukr.net>
2024-06-05 20:39:07 +0300

    Home Work 11 "Prettier, Eslint, Husky"

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import js from '@eslint/js';
+
+export default [
+    js.configs.recommended,
+    { languageOptions: { globals: globals.node } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+
+    {
+        rules: {
+            curly: 'error',
+            'getter-return': 'warn',
+            'constructor-super': 'error',
+            'no-dupe-args': 'warn',
+            'no-dupe-class-members': 'warn',
+            'no-unused-vars': 'error',
+            'no-self-assign': 'off',
+            'no-shadow-restricted-names': 'off',
+            'no-undef': 'error'
+        }
+    }
+];
